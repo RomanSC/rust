48005053b1489c0ab3e609c4742e4aeb67bc619e
RomanSC <r.s.col988@gmail.com>
2017-02-14 15:11:40 -0500

    Working on writing fibonacci function

---
+++ basics/expressions.rs
@@ -0,0 +1,28 @@
+fn main () {
+    // let n be equal to 5
+    let n = 5;
+
+    // operators
+    println!("Printing operators results.");
+    println!("{}", (n));
+    println!("{}", (n + 5));
+    println!("{}", (n - 5));
+    println!("{}", (n * 5));
+    println!("{}", (n / 5));
+    println!("{}", (n % 5));
+
+    // comparison
+    println!("Printing comparisons results.");
+    println!("{}", (n == 5));
+    println!("{}", (n != 5));
+    println!("{}", (n < 5));
+    println!("{}", (n > 5));
+    println!("{}", (n <= 5));
+    println!("{}", (n >= 5));
+}
+
+fn fib () -> (int) {
+    if (n == 0) || (n == 1) {
+        (int n)
+    }
+}
